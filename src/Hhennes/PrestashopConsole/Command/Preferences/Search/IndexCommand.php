c701c27878ec479e0e8773c65c28599d52a01359
lutek <m@lutek.pl>
2016-06-15 13:43:30 +0200

    command: add missing products to the index or re-build the entire index

--- src/Hhennes/PrestashopConsole/Command/Preferences/Search/IndexCommand.php
+++ src/Hhennes/PrestashopConsole/Command/Preferences/Search/IndexCommand.php
@@ -19,10 +19,10 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
  *
- *  @author    Hennes Hervé <contact@h-hennes.fr>
- *  @copyright 2013-2016 Hennes Hervé
- *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  http://www.h-hennes.fr/blog/
+ * @author    Mariusz Mielnik <mariusz@ecbox.pl>
+ * @copyright 2013-2016 Mariusz Mielnik
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  http://www.ecbox.pl
  */
 namespace Hhennes\PrestashopConsole\Command\Preferences\Search;
 
@@ -33,10 +33,44 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Commande qui permet de reindexer les produits prestashop
+ * Commands: Add missing products to the index or re-build the entire index
  *
  */
 class IndexCommand extends Command
 {
-    //put your code here
+
+    protected function configure()
+    {
+        $this
+            ->setName('preferences:search:index')
+            ->setDescription('Add missing products to the index or re-build the entire index (default)')
+            ->addArgument(
+                'type', InputArgument::OPTIONAL, 'add|rebuild(default)'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $type = $input->getArgument('type');
+
+        \Context::getContext()->shop->setContext(\Shop::CONTEXT_ALL);
+
+        switch ($type) {
+            case 'add':
+                $output->writeln('Adding missing products to the index... ');
+                \Search::indexation();
+                break;
+            case 'rebuild':
+            default:
+                $output->writeln('Re-building the entire ... ');
+                \Search::indexation(1);
+                break;
+        }
+
+        list($total, $indexed) = \Db::getInstance()->getRow('SELECT COUNT(*) as "0", SUM(product_shop.indexed) as "1" FROM ' . _DB_PREFIX_ . 'product p ' . \Shop::addSqlAssociation('product', 'p') . ' WHERE product_shop.`visibility` IN ("both", "search") AND product_shop.`active` = 1');
+
+        $output->writeln('Currently indexed products: ' . (int)$indexed . ' / ' . (int)$total);
+    }
+
 }
