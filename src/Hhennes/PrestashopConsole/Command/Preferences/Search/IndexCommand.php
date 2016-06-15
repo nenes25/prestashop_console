d078ad787ec770340ddf3aaf6d5eb1037b4b0661
lutek <m@lutek.pl>
2016-06-15 14:08:30 +0200

    Disable or enable shop command

--- src/Hhennes/PrestashopConsole/Command/Preferences/Search/IndexCommand.php
+++ src/Hhennes/PrestashopConsole/Command/Preferences/Search/IndexCommand.php
@@ -29,7 +29,6 @@ namespace Hhennes\PrestashopConsole\Command\Preferences\Search;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
