6b58d5eb1374116f2972e5e9d6df654733ad1ab3
Herve <nenes_fr@yahoo.fr>
2019-02-11 13:24:15 +0100

    Remove configuration file

--- .github/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Configuration du script de release
- */
-$github_user = 'nenes25';
-$github_password = 'herve25';
-$baseApiUrl = 'https://api.github.com/repos/nenes25/prestashop_console/';
-
