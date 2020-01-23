fb7ed9537956d8a079adf175ebe03556e98ad6f3
Hervé Hennes <nenes_fr@yahoo.fr>
2020-01-23 22:15:58 +0100

    #74 new commands to list hooks and hooks modules

---
+++ src/Hhennes/PrestashopConsole/Command/Hook/ModuleCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * 2007-2019 Hennes Hervé
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@h-hennes.fr so we can send you a copy immediately.
+ *
+ * @author    Hennes Hervé <contact@h-hennes.fr>
+ * @copyright 2007-2020 Hennes Hervé
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * http://www.h-hennes.fr/blog/
+ */
+
+
+namespace Hhennes\PrestashopConsole\Command\Hook;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use Hook;
+
+/**
+ * Class Module
+ * List hook with registered modules
+ */
+class ModuleCommand extends Command
+{
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('hook:modules')
+            ->setDescription('List all hooks with hooked modules');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        //Get Hooks list
+        $hooks = Hook::getHooks();
+
+        //Extract only hooks name
+        $hooks = array_map(function ($row) {
+            return $row['name'];
+        }, $hooks);
+
+        //Sort hooks by name
+        usort($hooks, array($this, "cmp"));
+
+        //Init Table
+        $table = new Table($output);
+        $table->setHeaders(['Hook Name', 'Modules hooked']);
+
+        foreach ($hooks as $hook) {
+
+            //Get Modules hooked
+            $hookModules = Hook::getHookModuleExecList($hook);
+
+            if ($hookModules) {
+
+                //Add module information on hook
+                $hookModulesInformations = '';
+                foreach ($hookModules as $index => $hookModule) {
+                    $hookModulesInformations .= ($index+1).".".$hookModule['module'].", ";
+                }
+                $table->addRow([$hook, trim($hookModulesInformations, ', ')]);
+            }
+        }
+
+        //Display result
+        $table->render();
+    }
+
+    /**
+     * Function to sort hook by name
+     * @param $a
+     * @param $b
+     * @return int|\lt
+     */
+    private function cmp($a, $b)
+    {
+        return strcmp($a, $b);
+    }
+}
