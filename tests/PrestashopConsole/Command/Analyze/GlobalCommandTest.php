307e65fdc6011837dffd8e215ab278a994405e42
Hervé Hennes <nenes_fr@yahoo.fr>
2025-11-06 21:38:41 +0100

    Add unit tests for ported commands
    Tests added for:
    - GlobalCommand: configuration and structure validation
    - TablesSizeCommand: options and arguments validation
    - ChangeShopDomainCommand: arguments and options validation
    
    All tests passing (6 tests, 14 assertions)

---
+++ tests/PrestashopConsole/Command/Analyze/GlobalCommandTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@h-hennes.fr so we can send you a copy immediately.
+ *
+ * @author    Hennes Hervé <contact@h-hennes.fr>
+ * @copyright since 2016 Hennes Hervé
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ *
+ * https://github.com/nenes25/prestashop_console
+ * https://www.h-hennes.fr/blog/
+ */
+
+use PrestashopConsole\Command\Analyze\GlobalCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use PHPUnit\Framework\TestCase;
+
+class GlobalCommandTest extends TestCase
+{
+    /** @var CommandTester */
+    private $commandTester;
+
+    /** @var Application */
+    private $application;
+
+    protected function setUp(): void
+    {
+        $this->application = new Application();
+        $this->application->add(new GlobalCommand());
+        
+        $command = $this->application->find('analyze:global');
+        $this->commandTester = new CommandTester($command);
+        
+        parent::setUp();
+    }
+
+    public function testCommandIsConfigured(): void
+    {
+        $command = $this->application->find('analyze:global');
+        
+        $this->assertEquals('analyze:global', $command->getName());
+        $this->assertEquals('Run a global analysis on the website', $command->getDescription());
+        $this->assertStringContainsString('meta command', $command->getHelp());
+    }
+
+    public function testCommandHasCorrectStructure(): void
+    {
+        $command = $this->application->find('analyze:global');
+        
+        // Verify the command is properly configured
+        $this->assertInstanceOf(GlobalCommand::class, $command);
+        $this->assertNotEmpty($command->getName());
+        $this->assertNotEmpty($command->getDescription());
+    }
+}
---
+++ tests/PrestashopConsole/Command/Analyze/TablesSizeCommandTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@h-hennes.fr so we can send you a copy immediately.
+ *
+ * @author    Hennes Hervé <contact@h-hennes.fr>
+ * @copyright since 2016 Hennes Hervé
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ *
+ * https://github.com/nenes25/prestashop_console
+ * https://www.h-hennes.fr/blog/
+ */
+
+use PrestashopConsole\Command\Analyze\TablesSizeCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use PHPUnit\Framework\TestCase;
+
+class TablesSizeCommandTest extends TestCase
+{
+    /** @var CommandTester */
+    private $commandTester;
+
+    /** @var Application */
+    private $application;
+
+    protected function setUp(): void
+    {
+        $this->application = new Application();
+        $this->application->add(new TablesSizeCommand());
+        
+        $command = $this->application->find('analyze:tables:size');
+        $this->commandTester = new CommandTester($command);
+        
+        parent::setUp();
+    }
+
+    public function testCommandIsConfigured(): void
+    {
+        $command = $this->application->find('analyze:tables:size');
+        
+        $this->assertEquals('analyze:tables:size', $command->getName());
+        $this->assertEquals('Analyze the size of the database tables sorted by size', $command->getDescription());
+    }
+
+    public function testCommandHasCorrectStructure(): void
+    {
+        $command = $this->application->find('analyze:tables:size');
+        
+        // Verify the command is properly configured
+        $this->assertInstanceOf(TablesSizeCommand::class, $command);
+        $this->assertNotEmpty($command->getName());
+        $this->assertNotEmpty($command->getDescription());
+    }
+
+    public function testCommandHasMinRowsOption(): void
+    {
+        $command = $this->application->find('analyze:tables:size');
+        $definition = $command->getDefinition();
+        
+        $this->assertTrue($definition->hasOption('min-rows'));
+        $option = $definition->getOption('min-rows');
+        $this->assertEquals('Show only tables with a minimum number of rows', $option->getDescription());
+    }
+
+    public function testCommandHasOutputFormatOption(): void
+    {
+        $command = $this->application->find('analyze:tables:size');
+        $definition = $command->getDefinition();
+        
+        $this->assertTrue($definition->hasOption('output'));
+        $option = $definition->getOption('output');
+        $this->assertEquals('Output format (table or json)', $option->getDescription());
+    }
+}
